3155f2595a913a133b71c39d0229ba007b65872f
sergey1701 <sergeyblinov348@gmail.com>
2022-04-19 21:06:20 +0000

    First Deployment5

--- app/Weather/views.py
+++ app/Weather/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Starting")
+    return render(request, "base.html")
 
 # Create your views here.
