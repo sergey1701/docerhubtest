db9564e34d5829bcc2a5d05e4c393276aef58cd3
sergey1701 <sergeyblinov348@gmail.com>
2022-04-22 16:12:37 +0000

    Test Git Ignore File

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 manage.py collectstatic
-python3 manage.py migrate  
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput   
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
 # python manage.py run server 0.0.0.0:8000
